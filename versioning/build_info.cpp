6a74024e27969bc9fc65f0b8bfb6dc9cd2cc41b2
Ryan Hunter <robocop@lycos.co.uk>
2021-06-04 13:16:05 +0100

    Neatened Versioning

--- versioning/build_info.cpp
+++ versioning/build_info.cpp
@@ -6,7 +6,7 @@
 const char* build_info::authors = "Cheeseh, RoboCop, nosoop, caxanga334";
 const char* build_info::url = "http://rcbot.bots-united.com/";
 
-const char* build_info::long_version = "caxanga334/rcbot2@" GIT_COMMIT_HASH;
+const char* build_info::long_version = "APGRoboCop/rcbot2@" GIT_COMMIT_HASH;
 const char* build_info::short_version = "1.4 (apg-nosoop-caxanga334)-" GIT_COMMIT_SHORT_HASH;
 
 const char* build_info::date = __DATE__;
