eaa5220f6172c5896238c37e1a68fde344545178
Ryan Hunter <robocop@lycos.co.uk>
2023-05-31 21:07:03 +0100

    Use screen text for menus in TF2
    https://github.com/nosoop/rcbot2/commit/81de31aef017f792a5a68aad40168a31db315f87

--- utils/RCBot2_meta/bot_menu.cpp
+++ utils/RCBot2_meta/bot_menu.cpp
@@ -355,8 +355,13 @@ void CBotMenu ::render (CClient *pClient)
 
 	const char* pszCaption = getCaption(pClient, color);
 
+#if SOURCE_ENGINE == SE_TF2
+	debugoverlay->AddScreenTextOverlay(0.135f, 0.4f, 0, fUpdateTime, color.r, color.g, color.b, color.a, pszCaption);
+	debugoverlay->AddScreenTextOverlay(0.135f, 0.4f, 1, fUpdateTime, color.r, color.g, color.b, color.a, "----------------");
+#else
 	debugoverlay->AddTextOverlayRGB(vOrigin,0,fUpdateTime,color.r,color.g,color.b,color.a,pszCaption);
 	debugoverlay->AddTextOverlayRGB(vOrigin,1,fUpdateTime,color.r,color.g,color.b,color.a,"----------------");
+#endif
 /*
 	Vector screen;
 	Vector point = Vector(0,0,0);
